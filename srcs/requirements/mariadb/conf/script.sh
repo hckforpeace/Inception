38bfb26fb3f8694905bfc545538fb6e6896c1ecd
hckforpeace <pierbey@gmail.com>
2025-01-02 23:48:50 +0100

    maria

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,14 +1,20 @@
 # start mysql
-sed '28s/.*/bind-address            = 127.0.0.1/' 50-server.cnf > temp_file && mv temp_file 50-server.cnf
+sed '28s/.*/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf > /etc/mysql/mariadb.conf.d/temp_file && mv /etc/mysql/mariadb.conf.d/temp_file /etc/mysql/mariadb.conf.d/50-server.cnf
 
 export db_name=mydb
 export db_user=pierre
 export db_pwd=234
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'pbeyloun' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'pbeyloun' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILIGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+
+
+service mysql start 
+sleep 4
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
+mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
+
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
+
+mysql -u root -p'12345' -e "FLUSH PRIVILEGES;"
+
+/usr/bin/mysqld_safe
