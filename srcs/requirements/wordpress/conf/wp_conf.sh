44b56d587e4107b3beec28f38fe96d9454fca4ad
hckforpeace <pierbey@gmail.com>
2025-02-21 00:39:46 +0100

    redis done :)

--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -26,10 +26,9 @@ WP_ADMIN_P=$(cat /run/secrets/wp_adm_password); wp core install --url="$DOMAIN_N
 #create a new user with the given username, email, password and role
 WP_U_PASS=$(cat /run/secrets/wp_usr_password); wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 
+# defines WP_REDIS_HOST to redis which is the address of the container
+wp config set WP_REDIS_HOST 'redis' --add --allow-root
 
-#
-echo "define('FS_METHOD', 'direct');" >> /var/www/wordpress/wp-config.php
-# change listen port from unix socket to 9000
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 # create a directory for php-fpm
 mkdir -p /run/php
